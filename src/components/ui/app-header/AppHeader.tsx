c6a68b8999a61e1edc9e0db51bb5ef80525911a9
Maxtop1qq <maksimsvetlov0@gmail.com>
2026-07-30 15:15:51 +0300

    Добавили header

--- src/components/ui/app-header/AppHeader.tsx
+++ src/components/ui/app-header/AppHeader.tsx
@@ -1,21 +1,60 @@
 import { NavLink } from "react-router-dom";
-import { HeaderLogo } from "../../../shared/icon";
-
+import HeaderLogo from "../../../shared/icon/assets/HeaderLogo.svg";
+import like from "../../../shared/icon/assets/like.svg";
+import notification from "../../../shared/icon/assets/notification.svg";
+import chevronDown from "../../../shared/icon/assets/chevron-down.svg";
+import moon from "../../../shared/icon/assets/moon.svg";
+import styles from "./appHeader.module.css";
+import { useState } from "react";
+import { Input } from "../../../shared/ui/input";
 export function AppHeader() {
+  const [isAuth, setisAuth] = useState(false);
+  const [value, setValue] = useState("");
   return (
-    <header>
-      <img src="./HeaderLogo.svg" alt="" />
-      <nav>
+    <header className={styles.header}>
+      <img src={HeaderLogo} alt="" />
+      <nav className={styles.nav}>
         <NavLink to="/">О проекте</NavLink>
-        <div>
+        <div className={styles.wrapNav}>
           <NavLink to="/">Все навыки</NavLink>
-          <img src=""></img>
+          <img src={chevronDown}></img>
         </div>
       </nav>
-      <input type="search" id="" placeholder="Искать навык" />
-      <NavLink to="/">
-        <img src="" alt="" />
-      </NavLink>
+      <Input onchange={setValue} placeholder="Искать навык" type="search" />
+
+      {isAuth ? (
+        <div className={styles.wrapBtn}>
+          <button>
+            <img src={moon} alt="" />
+          </button>
+          <div className={styles.wrapAuth}>
+            <button className={styles.authBtn} onClick={() => setisAuth(true)}>
+              Войти
+            </button>
+            <button className={styles.regBtn} onClick={() => setisAuth(true)}>
+              Зарегистрироваться
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.wrapRigth}>
+          <div className={styles.wrapIcon}>
+            <button>
+              <img src={moon} alt="" />
+            </button>
+            <button>
+              <img src={notification} alt="" />
+            </button>
+            <NavLink to="/">
+              <img src={like} alt="" />
+            </NavLink>
+          </div>
+          <div>
+            <p>Максим</p>
+            <img src="image" alt="" />
+          </div>
+        </div>
+      )}
     </header>
   );
 }
